bb84e0502eb5860165ab3f361d89dd245bc8a799
yukihase <yukihasenaka@gmail.com>
2025-10-26 01:36:04 +0900

    feat: Terraform構成ファイルの追加と更新

---
+++ terraform/main.tf
@@ -0,0 +1,75 @@
+# --- Data Sources ---
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+# --- Network Resources ---
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = data.azurerm_resource_group.rg.location # locationはリソースグループに合わせる
+  resource_group_name = data.azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# --- Subnets ---
+resource "azurerm_subnet" "frontend" {
+  name                 = "snet-frontend-integration"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "appservice-delegation"
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
+}
+
+resource "azurerm_subnet" "backend" {
+  name                 = "snet-backend-integration"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "appservice-delegation"
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
+}
+
+resource "azurerm_subnet" "private_endpoints" {
+  name                                      = "snet-private-endpoints"
+  resource_group_name                       = data.azurerm_resource_group.rg.name
+  virtual_network_name                      = azurerm_virtual_network.vnet.name
+  address_prefixes                          = ["10.0.3.0/24"]
+}
+
+
+# --- Network Security Groups (NSGs) ---
+resource "azurerm_network_security_group" "nsg_backend" {
+  name                = "nsg-backend"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow_Frontend_to_Backend"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["80", "443"]
+    source_address_prefix      = azurerm_subnet.frontend.address_prefixes[0]
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_backend_assoc" {
+  subnet_id                 = azurerm_subnet.backend.id
+  network_security_group_id = azurerm_network_security_group.nsg_backend.id
+}
